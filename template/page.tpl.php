14cb3f31abef39287b14757ecf278d8df7c72ac4
AtomskJD <AtomskJD@gmail.com>
2014-02-04 14:14:12 +0600

    Новый релиз
    Больше семантики, больше гибкости

---
+++ template/page.tpl.php
@@ -0,0 +1,17 @@
+<?php var_dump($logo); ?>
+<?php print render($page['info_contact']); ?>
+<?php print render($page['main_menu']); ?>
+<?php print render($page['content']); ?>
+<?php print render($page['footer']); ?>
+
+<?php if ($messages) print $messages; ?>
+
+<?php if ($breadcrumb) print $breadcrumb; ?>
+    
+<?php if ($title) print "<h1>".$title."</h1>";?>
+
+<?php if ($tabs) print render($tabs); ?>
+
+<?php var_dump(drupal_is_front_page()); ?>
+&copy;<?php echo date("Y"); ?>
+<?php if($site_name || $site_slogan) echo $site_name . " " . $site_slogan; ?>
